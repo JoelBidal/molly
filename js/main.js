af36ebdad4cf1f31afde1b7918357d5dbe4cb16a
Floker <fedeloker@gmail.com>
2020-07-03 23:44:54 -0300

    changes on JS navegability

--- js/main.js
+++ js/main.js
@@ -24,53 +24,55 @@
 var state = 0;
 function adelante(){
     state ++;
+    console.log(state);
     if (state == 1){
-            document.getElementById('background-1').style.visibility='hidden';
-            document.getElementById('background-2', 'main-image-2').style.visibility='visible';
-            document.getElementById('main-image-2').style.display='block';
-        
-            document.getElementById('next-image-1').style.display='none';
-            document.getElementById('next-image-2').style.display='block';
-            
-            document.getElementById('line-2').style.opacity='100%';        
-    }
+            show1();
+        }
     
     if (state == 2){
-            document.getElementById('line-2').style.opacity='40%';
-        
-            document.getElementById('background-1').style.visibility='visible';
-            document.getElementById('background-2', 'main-image-2').style.visibility='hidden';
-            document.getElementById('main-image-2').style.display='none';
-        
-            document.getElementById('next-image-1').style.display='block';
-            document.getElementById('next-image-2').style.display='none';
-            state=0;
+            show2();
+            state -= 2;
+            
         }
-
 }
 
 
 function atras(){
+    if(state == 0)
+             state = 2;
     state --;
+    console.log(state);
     if (state == 1){
-            document.getElementById('background-1').style.visibility='hidden';
-            document.getElementById('background-2', 'main-image-2').style.visibility='visible';
-            document.getElementById('main-image-2').style.display='block';
-        
-            document.getElementById('next-image-1').style.display='none';
-            document.getElementById('next-image-2').style.display='block';
-            
-            document.getElementById('line-2').style.opacity='100%';        
+        console.log("state2");
+        show1();
+        state = 2;
     }
     
     if (state == 2){
-            document.getElementById('line-2').style.opacity='40%';
-        
-            document.getElementById('background-1').style.visibility='visible';
-            document.getElementById('background-2', 'main-image-2').style.visibility='hidden';
-            document.getElementById('main-image-2').style.display='none';
-        
-            document.getElementById('next-image-1').style.display='block';
-            document.getElementById('next-image-2').style.display='none';
+        console.log("state2");
+        show2();
     }
+}
+
+
+function show1(){
+        document.getElementById('background-1').style.visibility='hidden';
+        document.getElementById('background-2', 'main-image-2').style.visibility='visible';
+        document.getElementById('main-image-2').style.display='block';
+    
+        document.getElementById('next-image-1').style.display='none';
+        document.getElementById('next-image-2').style.display='block';
+        
+        document.getElementById('line-2').style.opacity='100%';   
+}
+
+function show2(){
+        document.getElementById('line-2').style.opacity='40%';
+        
+        document.getElementById('background-1').style.visibility='visible';
+        document.getElementById('background-2', 'main-image-2').style.visibility='hidden';
+        document.getElementById('main-image-2').style.display='none';
+    
+        document.getElementById('next-image-1').style.display='block';
+        document.getElementById('next-image-2').style.display='none';  
 }
